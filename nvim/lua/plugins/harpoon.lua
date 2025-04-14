2be7204da077709c9a133b2a71c61e626b079f03
Elizabeth <lizbh26@proton.me>
2025-04-14 17:46:48 -0600

    Harpoon can now delete saves from within explorer

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -12,19 +12,34 @@ return {
 		-- basic telescope configuration
 		local conf = require("telescope.config").values
 		local function toggle_telescope(harpoon_files)
-			local file_paths = {}
-			for _, item in ipairs(harpoon_files.items) do
-				table.insert(file_paths, item.value)
+			local make_file_finder = function()
+				local paths = {}
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(paths, item.value)
+				end
+
+				return require("telescope.finders").new_table({
+					results = paths,
+				})
 			end
 
 			require("telescope.pickers")
 				.new({}, {
 					prompt_title = "Harpoon",
-					finder = require("telescope.finders").new_table({
-						results = file_paths,
-					}),
+					finder = make_file_finder(),
 					previewer = conf.file_previewer({}),
 					sorter = conf.generic_sorter({}),
+					attach_mappings = function(prompt_buffer_number, map)
+						map("i", "<C-d>", function()
+							local state = require("telescope.actions.state")
+							local selected_entry = state.get_selected_entry()
+							local current_picker = state.get_current_picker(prompt_buffer_number)
+
+							table.remove(harpoon_files.items, selected_entry.index)
+							current_picker:refresh(make_file_finder())
+						end)
+						return true
+					end,
 				})
 				:find()
 		end
@@ -35,7 +50,16 @@ return {
 
 		mapfnkey("<leader>a", function()
 			harpoon:list():add()
+			vim.notify("file added to harpoon", 2)
 		end, "n", { desc = "Add window to harpoon list" })
+
+		mapfnkey("<leader>nh", function()
+			harpoon:list():next()
+		end, "n", { desc = "Next harpoon" })
+
+		mapfnkey("<leader>ph", function()
+			harpoon:list():prev()
+		end, "n", { desc = "Previous harpoon" })
 
 		mapfnkey("<A-1>", function()
 			harpoon:list():select(1)
